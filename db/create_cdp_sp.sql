5e7f2266b185a61388ac67dd498f134fb814c605
JPCatarino <jorge.catarino@ua.pt>
2019-05-31 15:50:02 +0100

    Add ListBettersPerBooker and ListAllBookers sps

--- db/create_cdp_sp.sql
+++ db/create_cdp_sp.sql
@@ -1,11 +1,23 @@
-use SGCasaDEApostas;
+--use SGCasaDEApostas;
+--go
+
+use p3g6;
 go
 
-CREATE PROCEDURE cdp.ListBettersPerBooker @ID_Booker  INT 
+
+-- Stored proc to list all Booker Names
+CREATE PROCEDURE cdp.ListBookerNames
 AS
-	IF @ID_Booker is NULL
+	SELECT Nome from cdp.casa_de_apostas;
+GO
+
+-- Stored Proc to list people who bet on a given booker 
+CREATE PROCEDURE cdp.ListBettersPerBooker @Name_Booker  VARCHAR(255)
+AS
+	IF @Name_Booker is NULL
 	BEGIN 
 		PRINT 'Insert the ID of the Booker'
-		RETURN
+		RETURN 0
 	END
-	SELECT Primeiro_Nome, Ultimo_Nome, Telemovel, NIF  FROM cdp.apostador 
+	SELECT Primeiro_Nome, Ultimo_Nome, Telemovel, NIF  FROM cdp.apostador LEFT JOIN cdp.aposta_em ON Nome_CAP = @Name_Booker GROUP BY Primeiro_Nome, Ultimo_Nome, Telemovel, NIF;
+GO
