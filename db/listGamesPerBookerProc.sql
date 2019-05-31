ba6472dac2b379a6cbd040aa8fc6b0da90069de4
Oscar Pimentel <oscarpimentel@ua.pt>
2019-05-31 18:51:40 +0100

    Inicio do procedure para listar jogos por casa

---
+++ db/listGamesPerBookerProc.sql
@@ -0,0 +1,18 @@
+--use SGCasaDEApostas;
+use p3g6;
+go
+
+
+--A procedure that lists the games for a given booker
+CREATE PROCEDURE cdp.ListGamesPerBooker @Name_Booker  VARCHAR(255)
+AS
+	IF @Name_Booker is NULL
+	BEGIN 
+		PRINT 'Insert the ID of the Booker'
+		RETURN 0
+	END
+	
+	--Lista as apostas
+	--TODO: das apostas sacar os jogos
+	SELECT * FROM cdp.aposta_normal LEFT JOIN cdp.aposta_em ON Nome_CAP = @Name_Booker GROUP BY ID, Descricao, Odds, DataHora;
+GO
