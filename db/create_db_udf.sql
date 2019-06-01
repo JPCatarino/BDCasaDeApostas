663ab33510d05b9f4a6f0d566400730c43adb548
JPCatarino <jorge.catarino@ua.pt>
2019-06-01 18:39:43 +0100

    Add udf to check if game is set between competition dates

--- db/create_db_udf.sql
+++ db/create_db_udf.sql
@@ -4,13 +4,30 @@ GO
 CREATE SCHEMA utils;
 go
 
+CREATE FUNCTION cdp.IsGameSetDuringCompetition(@GameDate DATETIME, @Comp_ID INT) RETURNS bit 
+AS
+BEGIN
+	DECLARE @CompDataInicio DATETIME;
+	SELECT @CompDataInicio = Data_Inicio FROM cdp.competicao WHERE ID = @Comp_ID;
+	DECLARE @CompDataFim DATETIME;
+	SELECT @CompDataFim = Data_Fim FROM cdp.competicao WHERE ID = @Comp_ID;
+
+	IF @GameDAte BETWEEN @CompDataInicio and @CompDataFim
+	BEGIN
+		RETURN 1
+	END
+	RETURN 0
+END
+GO
+
 -- Aux Function to check if a given parameter is not null
 CREATE FUNCTION utils.IsNullOrEmpty(@toCheck VARCHAR(MAX)) RETURNS bit AS
 BEGIN
-IF @toCheck IS NOT NULL AND LEN(@toCheck) > 0
+	IF @toCheck IS NOT NULL AND LEN(@toCheck) > 0
 	BEGIN
-	RETURN 0
+		RETURN 0
 	END
-RETURN 1
+	RETURN 1
 END
 GO
+
