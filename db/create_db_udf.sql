cf5a19bdb02893d051feff4424518f5b1e5de1cd
JPCatarino <jorge.catarino@ua.pt>
2019-06-03 18:29:33 +0100

    Add udf to generate referencias

--- db/create_db_udf.sql
+++ db/create_db_udf.sql
@@ -20,6 +20,15 @@ BEGIN
 END
 GO
 
+CREATE FUNCTION 
+
+CREATE FUNCTION cdp.AreAllGameBetsPaid(@Game_ID INT) RETURNS bit
+AS 
+BEGIN	
+	
+END
+GO
+
 
 -- Aux Function to check if a given parameter is not null
 CREATE FUNCTION utils.IsNullOrEmpty(@toCheck VARCHAR(MAX)) RETURNS bit AS
@@ -32,3 +41,19 @@ BEGIN
 END
 GO
 
+create view getNewID as select newid() as new_id;
+GO
+
+CREATE FUNCTION utils.generateReferencia() RETURNS VARCHAR(50) AS
+BEGIN
+	DECLARE @randomRef VARCHAR(MAX);
+	DECLARE @newID VARCHAR(MAX);
+
+	SELECT @newID = new_id from getNewID;
+	SET @randomRef = CONVERT(varchar(255), @newID);
+
+	RETURN @randomRef
+END
+GO
+
+-- DROP FUNCTION utils.generateReferencia;
